2f417b7d6a8356a95cffd36d8f4072873b113312
mianzhang39 <77833234+mianzhang39@users.noreply.github.com>
2021-04-03 01:45:10 +0800

    Updated image and list widget

--- src/components/widgets/heading-widget.js
+++ src/components/widgets/heading-widget.js
@@ -8,17 +8,6 @@ const HeadingWidget = ({widget, setWidget,editing}) => {
     <div>
 
 
-
-
-        {/*<label>Heading</label>*/}
-
-        {widget.size === 1 && <h1>{widget.text}</h1>}
-        {widget.size === 2 && <h2>{widget.text}</h2>}
-        {widget.size === 3 && <h3>{widget.text}</h3>}
-        {widget.size === 4 && <h4>{widget.text}</h4>}
-        {widget.size === 5 && <h5>{widget.text}</h5>}
-        {widget.size === 6 && <h6>{widget.text}</h6>}
-
         {
             editing &&
             <div>
@@ -51,7 +40,6 @@ const HeadingWidget = ({widget, setWidget,editing}) => {
         {
             !editing &&
             <>
-                {/*{widget.text}*/}
                 {widget.size === 1 && <h1>{widget.text}</h1>}
                 {widget.size === 2 && <h2>{widget.text}</h2>}
                 {widget.size === 3 && <h3>{widget.text}</h3>}
